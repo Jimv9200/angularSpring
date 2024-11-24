43bdf7f21fb5f1d3451cece195a16716dfc3f6d8
Jorge <jimv9200@gmail.com>
2024-11-24 18:53:23 -0500

    Primera versión del proyecto

---
+++ src/main/java/co/edu/uniquindio/mongoangular/MongoAngularApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.mongoangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uniquindio/mongoangular/MongoAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.mongoangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
